4d43baeb0ef84334f6876d742b7f5156ae50323c
«Andrey_Nord» <spglav@mail.ru>
2025-05-19 11:27:37 +0300

    lesson 03. Функция input().

---
+++ lesson 03.py
@@ -0,0 +1,14 @@
+# Функция input()
+
+a = input('Введите строку: ')
+print(a)
+print(type(a)) # <class 'str'>
+
+a = int(input('Введите целое число: '))
+print(a)
+print(type(a)) # <class 'int'>
+
+a = float(input('Введите число с дробями: '))
+print(a)
+print(type(a)) # <class 'float'>
+
